76398eedec622c4e59e9f0da82555649c234cc95
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2026-06-08 13:08:44 -0400

    returns signed gradient amplitude On branch dev 	modified:   get_grad_scale.m

--- matlab/+pulseg/get_grad_scale.m
+++ matlab/+pulseg/get_grad_scale.m
@@ -1,91 +1,49 @@
-% test_get_grad_scale.m
-% Unit tests for get_grad_scale.m
-% Run with: results = runtests('test_get_grad_scale'); table(results)
+function scale = get_grad_scale(g)
+% GET_GRAD_SCALE Returns the signed maximum amplitude of a Pulseq gradient
+%
+% Input:
+%   g - A Pulseq gradient structure (either trapezoid or arbitrary)
+%
+% Output:
+%   scale - The signed maximum amplitude (value with largest absolute value,
+%           preserving sign). If positive and negative peaks have equal
+%           magnitude, the positive value is returned.
+%
+% Supports:
+%   - Trapezoid gradients (g.type == 'trap')
+%   - Arbitrary gradients (g.type == 'grad')
 
-function tests = test_get_grad_scale
-    tests = functiontests(localfunctions);
-end
+    if ~isstruct(g)
+        error('get_grad_scale:invalidInput', 'Input must be a Pulseq gradient structure.');
+    end
 
-%% Trapezoid Tests
+    if ~isfield(g, 'type')
+        error('get_grad_scale:missingField', 'Gradient structure must have a ''type'' field.');
+    end
 
-function test_trap_positive(testCase)
-    g.type = 'trap';
-    g.amplitude = 20e3;
-    testCase.verifyEqual(get_grad_scale(g), 20e3);
-end
+    switch g.type
+        case 'trap'
+            if ~isfield(g, 'amplitude')
+                error('get_grad_scale:missingField', 'Trapezoid gradient must have an ''amplitude'' field.');
+            end
+            scale = g.amplitude;
 
-function test_trap_negative(testCase)
-    g.type = 'trap';
-    g.amplitude = -20e3;
-    testCase.verifyEqual(get_grad_scale(g), -20e3);
-end
+        case 'grad'
+            if ~isfield(g, 'waveform')
+                error('get_grad_scale:missingField', 'Arbitrary gradient must have a ''waveform'' field.');
+            end
+            pos_peak = max(g.waveform);
+            neg_peak = min(g.waveform);
 
-function test_trap_zero(testCase)
-    g.type = 'trap';
-    g.amplitude = 0;
-    testCase.verifyEqual(get_grad_scale(g), 0);
-end
+            if abs(pos_peak) >= abs(neg_peak)
+                scale = pos_peak;
+            else
+                scale = neg_peak;
+            end
 
-%% Arbitrary Gradient Tests
+        otherwise
+            error('get_grad_scale:unknownType', ...
+                'Unknown gradient type ''%s''. Expected ''trap'' or ''grad''.', g.type);
+    end
 
-function test_grad_positive_peak(testCase)
-    g.type = 'grad';
-    g.waveform = [-5e3, 10e3, 3e3, -2e3];
-    testCase.verifyEqual(get_grad_scale(g), 10e3);
-end
-
-function test_grad_negative_peak(testCase)
-    g.type = 'grad';
-    g.waveform = [5e3, -10e3, 3e3, 2e3];
-    testCase.verifyEqual(get_grad_scale(g), -10e3);
-end
-
-function test_grad_equal_magnitude_prefers_positive(testCase)
-    g.type = 'grad';
-    g.waveform = [-10e3, 10e3, 3e3];
-    testCase.verifyEqual(get_grad_scale(g), 10e3);
-end
-
-function test_grad_all_positive(testCase)
-    g.type = 'grad';
-    g.waveform = [1e3, 5e3, 3e3];
-    testCase.verifyEqual(get_grad_scale(g), 5e3);
-end
-
-function test_grad_all_negative(testCase)
-    g.type = 'grad';
-    g.waveform = [-1e3, -5e3, -3e3];
-    testCase.verifyEqual(get_grad_scale(g), -5e3);
-end
-
-function test_grad_single_element(testCase)
-    g.type = 'grad';
-    g.waveform = [7e3];
-    testCase.verifyEqual(get_grad_scale(g), 7e3);
-end
-
-%% Error Handling Tests
-
-function test_not_a_struct(testCase)
-    testCase.verifyError(@() get_grad_scale(42), 'get_grad_scale:invalidInput');
-end
-
-function test_missing_type_field(testCase)
-    g.amplitude = 10e3;
-    testCase.verifyError(@() get_grad_scale(g), 'get_grad_scale:missingField');
-end
-
-function test_invalid_type(testCase)
-    g.type = 'unknown';
-    testCase.verifyError(@() get_grad_scale(g), 'get_grad_scale:unknownType');
-end
-
-function test_trap_missing_amplitude(testCase)
-    g.type = 'trap';
-    testCase.verifyError(@() get_grad_scale(g), 'get_grad_scale:missingField');
-end
-
-function test_grad_missing_waveform(testCase)
-    g.type = 'grad';
-    testCase.verifyError(@() get_grad_scale(g), 'get_grad_scale:missingField');
 end
